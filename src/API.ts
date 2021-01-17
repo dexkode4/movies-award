8e178b515652eb7522760bf0ae1d14c436ee5d6c
dexkode4 <dexkode4@gmail.com>
2021-01-17 14:31:21 +0100

    remove unused code

--- src/API.ts
+++ src/API.ts
@@ -2,14 +2,12 @@ import axios from 'axios';
 
 
 export const getMovies = async (query: string) => {
-    let cancel;
     return await axios.get('http://www.omdbapi.com/'
         , {
             params: {
                 s: query,
                 apikey: process.env.REACT_APP_API_KEY
             },
-            cancelToken: new axios.CancelToken(c => cancel = c)
         }
     )
         .then((res) => {
